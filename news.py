85a3bb85251b69f86ad6d2c26deca212a91cf981
missong <550864544@qq.com>
2017-04-06 15:50:53 +0800

    Update news.py

--- news.py
+++ news.py
@@ -1,5 +1,9 @@
+# -*- coding: utf-8 -*-
 import scrapy
 import re
+import BaseSpider
+import SgItem
+import Selector
 
 
 class NewsSpider(scrapy.Spider):
@@ -12,4 +16,14 @@ class NewsSpider(scrapy.Spider):
             try:
                 new = re.findall(r"[i][d][=]\d{5}",href)[0]
                 url = 'http://news.hitwh.edu.cn/'+ new
-        pass
+                yield scrapy.Request(url, callback=self.parse_news)
+	    except:
+	        continue
+            
+    def parse_news(self,response):
+        sel = Selector(response)
+        sg = SgItem()  
+        sg['url'] = response.url  
+        sg['title'] = sel.xpath("//div[@id='newsTitle']").extract()  
+        sg['body'] = sel.xpath("//div[@id='content']//p//text()").extract()  
+        return sg  
