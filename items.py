cf26e9b5714fc24a912bc0c2c9c9162ba9030d0f
念歌 <shige.shi@alibaba-inc.com>
2024-06-11 20:04:03 +0800

    modified:   items.py

--- items.py
+++ items.py
@@ -4,12 +4,14 @@
 #
 # See documentation in:
 # http://doc.scrapy.org/en/latest/topics/items.html
+# test
 from scrapy.item import Item,Field
 
 
 class SgItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
+    # 哈哈哈哈哈哈
     title = Field()
     url = Field()
     body = Field()
